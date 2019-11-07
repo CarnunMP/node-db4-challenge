6a3685fc21da641366682e8a866ec8735ab848d6
CarnunMP <carnun@hotmail.co.uk>
2019-11-07 15:44:56 +0000

    fleshed out migration create-tables

--- data/migrations/20191107144729_create-tables.js
+++ data/migrations/20191107144729_create-tables.js
@@ -1,8 +1,43 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('recipes', table => {
+      table.increments('recipe_id');
+      table.string('recipe_name', 128);
+    })
+    .createTable('ingredients', table => {
+      table.increments('ingredient_id');
+      table.string('ingredient_name', 128);
+      table.string('ingredient_unit', 128);
+    })
+    .createTable('steps', table => {
+      table.integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('recipe_id').inTable('recipes');
+      table.integer('step_number')
+        .unsigned()
+        .notNullable();
+      table.primary(['recipe_id', 'step_number']);
+      table.string('step_instruction', 256);
+    })
+    .createTable('ingredientMap', table => {
+      table.integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('recipe_id').inTable('recipes');
+      table.integer('ingredient_id')
+        .unsigned()
+        .notNullable()
+        .references('ingredient_id').inTable('ingredients');
+      table.decimal('quantity');
+    })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('ingredientMap')
+    .dropTableIfExists('steps')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipes');
 };
