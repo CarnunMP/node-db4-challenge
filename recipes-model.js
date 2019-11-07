83f3c9cfc5ea07d42bd1b48cd1528690af1f2eee
CarnunMP <carnun@hotmail.co.uk>
2019-11-07 20:02:11 +0000

    recipes-model.js done

---
+++ recipes-model.js
@@ -0,0 +1,31 @@
+const db = require('./data/db-config');
+
+module.exports = {
+  getRecipes,
+  getShoppingList,
+  getInstructions
+}
+
+function getRecipes() {
+  return db('recipes')
+    .select('recipes.recipe_name');
+}
+
+function getShoppingList(recipe_id) {
+  return db('recipes AS r')
+    .join('ingredientMap AS im', 'r.recipe_id', 'im.recipe_id')
+    .join('ingredients AS i', 'im.ingredient_id', 'i.ingredient_id')
+    .select(
+      'i.ingredient_name',
+      'im.quantity',
+      'i.ingredient_unit'
+    )
+    .where({ 'r.recipe_id': recipe_id });
+}
+
+function getInstructions(recipe_id) {
+  return db('steps AS s')
+    .select('s.step_intruction')
+    .where({ 's.recipe_id': recipe_id })
+    .orderBy('asc');
+}
